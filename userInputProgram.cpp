23890421362aa09d0e347586534359ec12360e7b
jakepalmer <jpalmer515@gmail.com>
2025-01-16 16:23:14 -0700

    Added .txt when calling the file Applied the inputString to the file Confirmed the appended string worked

--- userInputProgram.cpp
+++ userInputProgram.cpp
@@ -11,17 +11,17 @@ void fileInput(const std::string adminMsg) {
     //creating a file input/output stream using fstream because the file is already created
     //std::ios::app is used to append the file, adding the text at the end
     std::fstream fio;
-    fio.open("CSC450_CT5_mod5", std::ios::app);
+    fio.open("CSC450_CT5_mod5.txt", std::ios::app);
+    fio << inputString;
 
     //Create a pointer at the beginning of the file with an offset of 0
     fio.seekg(0, std::ios::beg);
 
-    //A while loop to print the file to the console, confirming the user input worked
-    // while (fio.good()) {
-    //     std::cout << inputString;
-    // }
+    //new string variable to hold the text file data and a fstream read function
     std::string fileText;
-    std::fstream readFio;
+    std::fstream readFio("CSC450_CT5_mod5.txt");
+
+    //A while loop to print the file to the console, confirming the user input worked
     while (getline (readFio, fileText)) {
         std::cout << fileText;
     }
