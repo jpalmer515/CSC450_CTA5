daccd619723b7f0b8cf5bcaebe5a2f7ce059c8f3
jakepalmer <jpalmer515@gmail.com>
2025-01-15 17:45:17 -0700

    Changed userInput to fileInput Finished v1 fileInput

--- userInputProgram.cpp
+++ userInputProgram.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 #include <fstream>
 
+void fileInput(const std::string adminMsg) {
+
+    std::string inputString;
+
+    std::cout << adminMsg;
+    std::cin >> inputString;
+
+    //creating a file input/output stream using fstream because the file is already created
+    //std::ios::app is used to append the file, adding the text at the end
+    std::fstream fio;
+    fio.open("CSC450_CT5_mod5", std::ios::app);
+
+    //Create a pointer at the beginning of the file with an offset of 0
+    fio.seekg(0, std::ios::beg);
+
+    //A while loop to print the file to the console, confirming the user input worked
+    while (fio.good()) {
+        std::cout << inputString;
+    }
+    fio.close();
+}
+
+void textReverse(std::string userInputs) {
+
+}
+
 int main() {
 
-    std::string userInput;
-
-    std::cout << "Please input a string: ";
-    std::cin >> userInput;
-
-}
-
-void userInput() {
-
-    std::cout << userInput << std::endl;
-
-}
-
-void textReverse(std::string userInput) {
+    fileInput("Please enter a string to be added to the text file. ");
 
 }
