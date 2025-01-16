9706341e07e53749c09bee9e2076be159f373869
jakepalmer <jpalmer515@gmail.com>
2025-01-15 17:29:23 -0700

    Added a userInput function.

---
+++ userInputProgram.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+
+    std::string userInput;
+
+    std::cout << "Please input a string: ";
+    std::cin >> userInput;
+
+}
+
+void userInput() {
+
+    std::cout << userInput << std::endl;
+
+}
+
+void textReverse(std::string userInput) {
+
+}
