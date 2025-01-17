0d664f8f0e551b3f340301070e31b31e01f4d242
jakepalmer <jpalmer515@gmail.com>
2025-01-16 17:05:46 -0700

    Troubleshooting reverse function not reversing the entire text file

--- userInputProgram.cpp
+++ userInputProgram.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 
@@ -11,30 +12,48 @@ void fileInput(const std::string adminMsg) {
     //creating a file input/output stream using fstream because the file is already created
     //std::ios::app is used to append the file, adding the text at the end
     std::fstream fio;
-    fio.open("CSC450_CT5_mod5.txt", std::ios::app);
+    fio.open("C:/Users/Jake/OneDrive/Desktop/Folder/School/CSU Global/CSC450 - Programming 3/CSC450_CT5_mod5.txt", std::ios::app);
     fio << inputString;
 
-    //Create a pointer at the beginning of the file with an offset of 0
+    //create a pointer at the beginning of the file with an offset of 0
     fio.seekg(0, std::ios::beg);
 
     //new string variable to hold the text file data and a fstream read function
     std::string fileText;
-    std::fstream readFio("CSC450_CT5_mod5.txt");
+    std::fstream readFio("C:/Users/Jake/OneDrive/Desktop/Folder/School/CSU Global/CSC450 - Programming 3/CSC450 CTA5CSC450_CT5_mod5.txt");
 
-    //A while loop to print the file to the console, confirming the user input worked
+    //a while loop to print the file to the console, confirming the user input worked
     while (getline (readFio, fileText)) {
         std::cout << fileText;
     }
     fio.close();
 }
 
-// void textReverse() {
-//
-// }
+void textReverse() {
+
+    std::string reversedString;
+    std::fstream readFio("C:/Users/Jake/OneDrive/Desktop/Folder/School/CSU Global/CSC450 - Programming 3/CSC450 CTA5CSC450_CT5_mod5.txt");
+    readFio.seekg(0, std::ios::beg);
+
+    while (getline (readFio, reversedString)) {
+        readFio >> reversedString;
+    }
+    std::cout << reversedString;
+
+    //built in C++ function to reverse data or elements (applies the reversal to the original variable)
+    reverse(reversedString.begin(), reversedString.end());
+    std::cout << "\n" << reversedString;
+
+    //create a new file and write the reversedString to it
+    std::fstream reversedFio("C:/Users/Jake/OneDrive/Desktop/Folder/School/CSU Global/CSC450 - Programming 3/CSC450_CT5_mod5_reverse.txt");
+    reversedFio << reversedString;
+
+    reversedFio.close();
+}
 
 int main() {
 
     fileInput("Please enter a string to be added to the text file. \n");
-    //textReverse();
+    textReverse();
 
 }
